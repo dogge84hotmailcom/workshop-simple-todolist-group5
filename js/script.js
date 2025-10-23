d70f3ac8000d07451f95b7f5f9ce8ebb1dca6306
dogge84@hotmail.com <dogge84@hotmail.com>
2025-10-23 11:36:44 +0200

    first commit group 5

--- js/script.js
+++ js/script.js
@@ -1,25 +1,41 @@
 // 1. DOM referenser
 const todoList = document.getElementById("todos");
 
+const button = document.getElementById("addBtn");
+
+const input = document.getElementById("input");
+
 
 
 // 2. Eventlyssnare - När ett formulär submittas anropa funktion addTodo()
 
-
+button.addEventListener("click", (event) => addTodo(event));
 
 
 
 // 3. Börja med en tom array på todos
 // samt ett id som börjar på noll
 
-
-
+let todos = [];
+let currentId = 0;
 
 
 // 4. Lägg till en Todo som ett objekt  i arrayen todos
-function addTodo() {
+function addTodo(event) {
 
-    
+    event.preventDefault();
+
+    const todo = {
+        id : currentId + 1,
+        task : input.value,
+        completed : false
+    };
+
+    todos.push(todo)
+    currentId += 1;  
+
+    input.value = "";
+    renderList()
 }
 
 
@@ -27,9 +43,25 @@ function addTodo() {
 // 5. Funktion som renderar listan på index.html varje gång listan förändraas
 function renderList() {
 
+    todoList.innerHTML = "";
+    
+    todos.forEach(todo => {
+
+        todoList.innerHTML += `
+            <li id="${todo.id}" class="${todo.completed ? "completed" : ""}">
+                <span >${todo.task}</span>
+                <button >X</button>
+            </li>
+        `
+    })
+
+    console.log(todos)
+
     // Iterera genom varje list-element och skapa en <li></li> med todo-texten, samt en close-knapp
 
 }
+
+
 
 
 
@@ -39,8 +71,16 @@ todoList.addEventListener('click', function(event) {
         // Element som klickades på i listan
         console.log(event.target);
 
-        // Om man texten klickas => anropa toggleTodoCompletion
+        if(event.target.tagName == "SPAN"){
+            // Om man texten klickas => anropa toggleTodoCompletion
+            toggleTodoCompletion(event.target.parentElement.id)
+        } else if(event.target.tagName == "BUTTON"){
+            deleteTodo(event.target.parentElement.id)
+        }
 
+
+
+        
         // Eller om close-knappen klickas => anrop deleteTOto
 
 });
@@ -48,14 +88,25 @@ todoList.addEventListener('click', function(event) {
 
 // Bonus! Uppdatera en todo med visst id. Toggle "completed"
 function toggleTodoCompletion(id) {
-
-
+    const item = todos.find(i => i.id == id)
+    if (item.completed){
+        item.completed = false;
+    }
+    else {
+        item.completed = true;
+    }
+    renderList()
 }
 
 
 
 // Bonus
 function deleteTodo(id) {
+
+    const item = todos.find(i => i.id == id)
+   
+    todos = todos.filter(todo => todo != item)
+    renderList();
   
 }
 
